4907e34c5b204be8f3d0376c06afb2f47d397a51
novy <45167857+novysodope@users.noreply.github.com>
2022-05-19 11:17:30 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
+#!coding:utf-8
 import sys
 import threading
 import requests
+import signal
 import random
 from queue import Queue
 from optparse import OptionParser
 from colorama import Fore,Back,Style
-import io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
+# import io
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 import urllib.request
 import re
+import time
+# import importlib,sys
+# importlib.reload(sys)
 
 def agent_list():
   agents = [
@@ -31,6 +36,7 @@ def agent_list():
     {'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_2; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.366.0 Safari/533.4'},
     {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.51'}]
 
+
   return random.choice(agents)
 
 
@@ -45,11 +51,11 @@ def main():
 .##....##..##..##..##.##.....##.##....##.##....##.##.....##.##...###
 .##.....##..###..###..########...######...######..##.....##.##....##
                                           
- Welcome use Laji Jiaoben,I'm lunzi king
- Rich Woman Directory Scanner --富婆扫描器 v2.0.1
+ 扫描结束后会在目录下生成相关结果，请自行查看
+ Rich Woman Directory Scanner --富婆目录扫描器 v2.0.2
  by: novy\n\n'''
  print(Fore.BLUE + author)
- parser = OptionParser('python run.py -u <目标URL> -f <字典文件> [-t <线程数，这个参数可有可无>]，\n\n示例：\npython3 run.py -u http://example.com -f dict.txt')
+ parser = OptionParser('python run.py -u <目标URL> -f <字典文件> [-t <线程数，不输入默认10>]，\n\n示例：\npython3 run.py -u http://example.com -f dict.txt')
  parser.add_option('-u', dest='url', type='string', help='要扫描的目标')
  parser.add_option('-f', dest='file_name', type='string', help='字典文件')
  parser.add_option('-t', dest='count', type='int', default=10, help='线程数')
@@ -61,7 +67,11 @@ def main():
  else:
    parser.print_help()
    sys.exit(1)
-
+# try:
+#   while 1:
+#     pass
+# except KeyboardInterrupt:
+#   quit()
 class RWALScan:
   def __init__(self, options):
     self.url = options.url
@@ -78,148 +88,148 @@ class RWALScan:
       self._total = total
 
     def run(self):
+
       while not self._queue.empty():
         url = self._queue.get()
-        # 多线程显示进度
-        threading.Thread(target=self.msg).start()
+        # # 多线程显示进度
+        # threading.Thread(target=self.msg).start()
         xxx = agent_list()
         try:
-          r = requests.get(url=url, headers=xxx, timeout=5)
-          r.headers
-          ad = r.headers.get('Server')
-          ab = r.headers.get('X-Powered-By')
-          #ap = r.headers.get('Location')
-          # an = r.headers.get('Set-Cookie')
-          if ad:
+          r = requests.get(url=url,headers=xxx,timeout=5)
+          code=r.status_code
+          Server = r.headers.get('Server')
+          if Server:
             pass
           else:
-            ad = "null"
-          if ab:
+            Server = 'null'
+
+          ContentLength = r.headers.get('Content-Length')
+          if ContentLength:
             pass
           else:
-            ab = "null"
-          ac = r.headers.get('Content-Length')
-          if ac:
-          	pass
+            ContentLength = 'null'
+
+          Location = r.headers.get('Location')
+          if Location:
+            pass
           else:
-          	ac = "null"
-          # if ap:
-          #   pass
-          # else:
-          #   ap = "null"
-          #print(ab)
-          # if an:
-          #   pass
-          # else:
-          #   an = "null"
-          if r.status_code == 200:
-            page = urllib.request.urlopen(url)
-            html = page.read().decode('utf-8')
-            # Python3 findall数据类型用bytes类型
-            # or html=urllib.urlopen(url).read()
+            Location = 'null'
+
+          if code==200:
+            html = r.content.decode("utf-8")
             title=re.findall('<title>(.+)</title>',html)
             if title:
               pass
             else:
-              title = ['null']
-            print('\r' + Fore.GREEN + '[+]%s\t\t\n' % url)
-            #print (title)
+              title=re.findall('\"error\"\:\"(.+)\",\"message\"',html)
+              if title:
+                pass
+              else:
+                title = ['标题获取异常']
+            print('\r%s '% code+'[+]%s\t' % url+'|'+title[0]+'\n')
             result = open('result-200.html', 'a+')
-            result.write('<title>200-富婆扫描器</title>页面存在！请点击下面链接验证<table><tr><td><a href="' + url + '" ' + 'target="_blank">' + url + '</a>&nbsp;</font><br><a>网站标题：[<font color="#FF0000">' + title[0] + '</font>]&nbsp;&nbsp;服务器环境：[<font color="#FF0000">' + ad + '</font>]</a>&nbsp;&nbsp;<br><a>X-Powered-By：[<font color="FF0000">' + ab + '</font>]&nbsp;&nbsp;Length：[<font color="FF0000">' + ac + '</font>]</a></td></tr></table>')
+            result.write('<title>200-富婆扫描器</title>200页面：<table><tr><td><a href="' + url + '" ' + 'target="_blank">' + url + '</a>&nbsp;</font><br><a>网站标题：[<font color="#FF0000">' + title[0] + '</font>]&nbsp;&nbsp;服务器环境：[<font color="#FF0000">' + Server + '</font>]</a>&nbsp;&nbsp;<br><a>Length：[<font color="FF0000">' + ContentLength + '</font>]</a></td></tr></table>')
             result.write('\r\n</br>')
             result.close()
-          if r.status_code == 404:
-            result = open('result-404.html', 'a+')
-            result.write('<title>404-富婆扫描器</title>404页面<table><tr><td><a href="' + url + '" ' +'target="_blank">' + url + '</a>&nbsp;<font><br><a>标题：[<font color="#FF0000">' + title[0] + '</font>]&nbsp;&nbsp;服务器环境：[<font color="#FF0000">' + ad + '</font>]</a>&nbsp;&nbsp;<br><a>X-Powered-By：[<font color="FF0000">' + ab + '</font>]&nbsp;&nbsp;Length：[<font color="FF0000">' + ac + '</font>]</a></td></tr></table>')
-            result.write('\r\n<br>')
-            result.close()
-          if r.status_code == 405:
-            result = open('result-405.html', 'a+')
-            result.write('<title>404-富婆扫描器</title>不允许GET请求，请换种请求方式：<table><tr><td><a href="' + url + '" ' + 'target="_blank">' + url + '</a>&nbsp;<font><br><a>标题：[<font color="#FF0000">' + title[0] + '</font>]&nbsp;&nbsp;服务器环境：[<font color="#FF0000">' + ad + '</font>]</a>&nbsp;&nbsp;<br><a>X-Powered-By：[<font color="FF0000">' + ab + '</font>]&nbsp;&nbsp;Length：[<font color="FF0000">' + ac + '</font>]</a></td></tr></table>')
-            result.write('\r\n<br>')
-            result.close()
-          if r.status_code == 500:
-            result = open('result-500.html', 'a+')
-            result.write('<title>404-富婆扫描器</title>500页面<table><tr><td><a href="' + url + '" ' + 'target="_blank">' + url + '</a>&nbsp;<font><br><a>标题：[<font color="#FF0000">' + title[0] + '</font>]&nbsp;&nbsp;服务器环境：[<font color="#FF0000">' + ad + '</font>]</a>&nbsp;&nbsp;<br><a>X-Powered-By：[<font color="FF0000">' + ab + '</font>]&nbsp;&nbsp;Length：[<font color="FF0000">' + ac + '</font>]</a></td></tr></table>')
-            result.write('\r\n<br>')
-            result.close()
-            #print('[!]异常')
-            #return r.raise_for_status
-          '''
-          if r.status_code == 404:
-            print('\r' + Fore.RED + '[404]%s\t\t\n' % url)
-          if r.status_code == 500:
-            print('\r' + Fore.YELLOW + '[500]%s\t\t\n' % url)
-            result = open('result-500.html', 'a+')
-            result.write('<title>富婆地址列表扫描结果</title>似乎是缺少了参数，请点击下面链接查看详情<table><tr><td><a href="' + url + '" rel="external nofollow" target="_blank">' + url + '</a></td></tr></table>')
-            result.write('\r\n</br>')
-            result.close()
-          if r.status_code == 302:
-            print('\r' + Fore.YELLOW + '[302]%s\t\t\n' % url)
+
+          if code==302:
+            html = r.content.decode("utf-8")
+            title=re.findall('<title>(.+)</title>',html)
+            if title:
+              pass
+            else:
+              title=re.findall('\"error\"\:\"(.+)\",\"message\"',html)
+              if title:
+                pass
+              else:
+                title = ['标题获取异常']
+            # print('\r\n%s '% code+'[+]%s' % url+'   '+title[0])
             result = open('result-302.html', 'a+')
-            result.write('<title>富婆地址列表扫描结果</title>被转发了，请点击下面链接查看详情<table><tr><td><a href="' + url + '" rel="external nofollow" target="_blank">' + url + '</a></td></tr></table>')
+            result.write('<title>302-富婆扫描器</title>302页面：<table><tr><td><a href="' + url + '" ' + 'target="_blank">' + url + '</a>&nbsp;</font><br><a>网站标题：[<font color="#FF0000">' + title[0] + '</font>]&nbsp;&nbsp;服务器环境：[<font color="#FF0000">' + Server + '</font>]</a>&nbsp;&nbsp;<br><a>Length：[<font color="FF0000">' + ContentLength + '</font>]</a>&nbsp;&nbsp;<br><a>Location：[<font color="FF0000">' + Location + '</font>]</a></td></tr></table>')
             result.write('\r\n</br>')
             result.close()
-          if r.status_code == 304:
-            print('\r' + Fore.YELLOW + '[304]%s\t\t\n' % url)
-            result = open('result-304.html', 'a+')
-            result.write('<title>富婆地址列表扫描结果</title>被转发了，请点击下面链接查看详情<table><tr><td><a href="' + url + '" rel="external nofollow" target="_blank">' + url + '</a></td></tr></table>')
-            result.write('\r\n</br>')
-            result.close()
-          if r.status_code == 303:
-            print('\r' + Fore.YELLOW + '[303]%s\t\t\n' % url)
-            result = open('result-303.html', 'a+')
-            result.write('<title>富婆地址列表扫描结果</title>被转发了，请点击下面链接查看详情<table><tr><td><a href="' + url + '" rel="external nofollow" target="_blank">' + url + '</a></td></tr></table>')
-            result.write('\r\n</br>')
-            result.close()
-          if r.status_code == 305:
-            print('\r' + Fore.YELLOW + '[305]%s\t\t\n' % url)
-            result = open('result-305.html', 'a+')
-            result.write('<title>富婆地址列表扫描结果</title>被转发了，请点击下面链接查看详情<table><tr><td><a href="' + url + '" rel="external nofollow" target="_blank">' + url + '</a></td></tr></table>')
-            result.write('\r\n</br>')
-            result.close()
-          if r.status_code == 301:
-            print('\r' + Fore.YELLOW + '[301]%s\t\t\n' % url)
-            result = open('result-301.html', 'a+')
-            result.write('<title>富婆地址列表扫描结果</title>被转发了，请点击下面链接查看详情<table><tr><td><a href="' + url + '" rel="external nofollow" target="_blank">' + url + '</a></td></tr></table>')
-            result.write('\r\n</br>')
-            result.close()
-          if r.status_code == 403:
-            print('\r' + Fore.RED + '[403]%s\t\t\n' % url)
-          if r.status_code == 405:
-            print('\r' + Fore.YELLOW + '[405]%s\t\t\n' % url)
+
+          if code==405:
+            html = r.content.decode("utf-8")
+            title=re.findall('<title>(.+)</title>',html)
+            if title:
+              pass
+            else:
+              title=re.findall('\"error\"\:\"(.+)\",\"message\"',html)
+              if title:
+                pass
+              else:
+                title = ['标题获取异常']
+            print('\r\n%s '% code+'[+]%s\t' % url+'|'+title[0]+'\n')
             result = open('result-405.html', 'a+')
-            result.write('<title>富婆地址列表扫描结果</title>此页面似乎不支持get请求，请点击下面链接查看详情，并换一种请求方法<table><tr><td><a href="' + url + '" rel="external nofollow" target="_blank">' + url + '</a></td></tr></table>')
+            result.write('<title>405-富婆扫描器</title>405页面：<table><tr><td><a href="' + url + '" ' + 'target="_blank">' + url + '</a>&nbsp;</font><br><a>网站标题：[<font color="#FF0000">' + title[0] + '</font>]&nbsp;&nbsp;服务器环境：[<font color="#FF0000">' + Server + '</font>]</a>&nbsp;&nbsp;<br><a>Length：[<font color="FF0000">' + ContentLength + '</font>]</a></td></tr></table>')
             result.write('\r\n</br>')
             result.close()
-          '''
+
+          if code==500:
+            html = r.content.decode("utf-8")
+            title=re.findall('<title>(.+)</title>',html)
+            if title:
+              pass
+            else:
+              title=re.findall('\"error\"\:\"(.+)\",\"message\"',html)
+              if title:
+                pass
+              else:
+                title = ['标题获取异常']
+            print('\r\n%s '% code+'[+]%s\t' % url+'|'+title[0]+'\n')
+            result = open('result-500.html', 'a+')
+            result.write('<title>500-富婆扫描器</title>500页面：<table><tr><td><a href="' + url + '" ' + 'target="_blank">' + url + '</a>&nbsp;</font><br><a>网站标题：[<font color="#FF0000">' + title[0] + '</font>]&nbsp;&nbsp;服务器环境：[<font color="#FF0000">' + Server + '</font>]</a>&nbsp;&nbsp;<br><a>Length：[<font color="FF0000">' + ContentLength + '</font>]</a></td></tr></table>')
+            result.write('\r\n</br>')
+            result.close()
+
+          if code==400:
+            html = r.content.decode("utf-8")
+            title=re.findall('<title>(.+)</title>',html)
+            if title:
+              pass
+            else:
+              title=re.findall('\"error\"\:\"(.+)\",\"message\"',html)
+              if title:
+                pass
+              else:
+                title = ['标题获取异常']
+            # print('\r\n%s '% code+'[+]%s' % url+'   '+title[0])
+            result = open('result-400.html', 'a+')
+            result.write('<title>400-富婆扫描器</title>400页面：<table><tr><td><a href="' + url + '" ' + 'target="_blank">' + url + '</a>&nbsp;</font><br><a>网站标题：[<font color="#FF0000">' + title[0] + '</font>]&nbsp;&nbsp;服务器环境：[<font color="#FF0000">' + Server + '</font>]</a>&nbsp;&nbsp;<br><a>Length：[<font color="FF0000">' + ContentLength + '</font>]</a></td></tr></table>')
+            result.write('\r\n</br>')
+            result.close()
         except Exception:
-          pass
+          break
+        except Exception as e:
+          print('程序发生错误，请根据异常检查：'+e)
+          sys.exit()
 
 
-    def msg(self):
-      per = 100 - float(self._queue.qsize()) / float(self._total) * 100
-      percent = "已扫描 %s 个| 总共 %s 个| 已完成 %1.f %s" % (
-        (self._total - self._queue.qsize()), self._total, per, '%')
-      sys.stdout.write('\r' + '[*]' + percent)
+    # def msg(self):
+    #   per = 100 - float(self._queue.qsize()) / float(self._total) * 100
+    #   percent = "已扫描 %s 个| 总共 %s 个| 已完成 %1.f %s" % (
+    #     (self._total - self._queue.qsize()), self._total, per, '%')
+    #   sys.stdout.write('\r' + '[*]' + percent)
 
   def start(self):
     queue = Queue()
-    f = open('dict.txt', 'r')
+    f = open(sys.argv[4] , 'r')
     for i in f.readlines():
-    	#要是字典路径前面缺少/，加个/，最好是字典路径自己加/，比如/index.php
       queue.put(self.url + i.rstrip('\n'))
     total = queue.qsize()
     threads = []
     thread_count = int(self.count)
-    for i in range(thread_count):
-      threads.append(self.PathScan(queue, total))
-    for thread in threads:
-      thread.start()
-    for thread in threads:
-      thread.join()
-
+    try:
+      for i in range(thread_count):
+        threads.append(self.PathScan(queue, total))
+      for thread in threads:
+        thread.start()
+      for thread in threads:
+        thread.join()
+    except Exception:
+          sys.exit(1)
+ 
 
 if __name__ == '__main__':
-
    main()
